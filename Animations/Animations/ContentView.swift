b1ba73bda7f28de26c12ec8ac7f5ac2f4a402664
Juan Dorado <jfdoradotr@icloud.com>
2024-11-14 17:23:57 -0600

    Specify the type of animation on the modifier, the modifier attaches to the value

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,9 +8,16 @@ struct ContentView: View {
   @State private var animationAmount = 1.0
 
   var body: some View {
-    print(animationAmount)
-    return VStack {
-      Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+    VStack {
+      Stepper(
+        "Scale amount",
+        value: $animationAmount
+          .animation(
+            .easeInOut(duration: 1)
+            .repeatCount(3, autoreverses: true)
+          ),
+        in: 1...10
+      )
 
       Spacer()
 
