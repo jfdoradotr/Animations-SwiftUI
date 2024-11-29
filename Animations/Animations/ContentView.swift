3b877f1b5d8e040084883860d65b205d1370958e
Juan Dorado <jfdoradotr@icloud.com>
2024-11-29 09:46:05 -0600

    Create a custom modifier to use it as a transition

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -4,6 +4,17 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+  let amount: Double
+  let anchor: UnitPoint
+
+  func body(content: Content) -> some View {
+    content
+      .rotationEffect(.degrees(amount), anchor: anchor)
+      .clipped()
+  }
+}
+
 struct ContentView: View {
   @State private var isShowingRed = false
 
